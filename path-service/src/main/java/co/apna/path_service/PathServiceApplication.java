b7507909613de2797c79864a6b217d86a58bba82
Ashutosh Pathak <ashu.p89@gmail.com>
2025-04-24 23:56:28 +0530

    path service - building successfully

---
+++ path-service/src/main/java/co/apna/path_service/PathServiceApplication.java
@@ -0,0 +1,13 @@
+package co.apna.path_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathServiceApplication.class, args);
+	}
+
+}
---
+++ path-service/src/test/java/co/apna/path_service/PathServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.apna.path_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
